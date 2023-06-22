27f93f959b7146da8c4235c85c43a31d370f9672
Polina <polina5523312@gmail.com>
2023-06-22 16:22:57 +0500

    sign in

--- src/components/forms/FormAuthorization.jsx
+++ src/components/forms/FormAuthorization.jsx
@@ -1,23 +1,76 @@
+import { useState } from 'react'
 import css from './Forms.module.css'
 
-const FormAuthorization = () => {
+const FormAuthorization = props => {
+
+	const [values, setValues] = useState({
+		email: '',
+		password: ''
+	})
+
+	const handleChange = e => {
+		const fieldName = e.target.name
+		setValues({...values, [fieldName]: e.target.value})
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		if (values.email) {
+			formSubmit(values.email, values.password)
+		}
+	}
+
+	const SignIn = () => {
+        let axios = require('axios');
+
+		let config = {
+			method: 'post',
+		  	maxBodyLength: Infinity,
+			url: 'https://sf-final-project-be.herokuapp.com/api/auth/sign_in',
+			headers: { },
+			data : {
+				"email": "polina5523312@gmail.com",
+				"password": "5523312",
+			}
+		  };
+
+		  axios(config)
+			.then(function (response) {
+  				console.log(JSON.stringify(response.data));
+			})
+			.catch(function (error) {
+  				console.log(error);
+			});
+    }
+
+	const formSubmit = (email, password) => {
+		console.log('ok')
+		SignIn()
+	}
+
 	return (
         <div className={css.form}>
-            <form className={css.formAuthorization}>
+            <form onSubmit={handleSubmit} formSubmit={formSubmit} className={css.formAuthorization}>
                 <p className={css.formName}>Авторизация</p>
 
                 <p className={css.inputText}>E-mail* <input 
 				className={css.input}
-				type="email" 
-				name="email" 
-				placeholder="E-mail">
+				id='auth'
+				type='email'
+				name='email'
+				placeholder='E-mail'
+				onChange={handleChange}
+				value={values.email}>
 				</input></p>
 						
 				<p className={css.inputText}>Пароль* <input 
 				className={css.input}
-				type="password" 
-				name="password" 
-				placeholder="Введите пароль">
+				id='pass'
+				type='password'
+				name='password'
+				placeholder='Введите пароль'
+				onChange={handleChange}
+				value={values.password}>
 				</input></p>
 
 				<button className={css.submit} type='submit'>Войти</button>
--- src/components/pages/authorization/AuthorizationPage.jsx
+++ src/components/pages/authorization/AuthorizationPage.jsx
@@ -3,6 +3,7 @@ import FormAuthorization from '../../forms/FormAuthorization'
 import css from './AuthorizationPage.module.css'
 
 const AuthorizationPage = () => {
+
     return (
         <>
             <Link to='/staff'className={css.homeLink}>&#8592; Назад</Link>
