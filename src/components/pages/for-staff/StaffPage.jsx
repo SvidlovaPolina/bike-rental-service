f8abc70c3b712746ebd72f828a04bf1d86025e61
Polina <polina5523312@gmail.com>
2023-06-30 01:49:34 +0500

    put +edit

--- src/components/pages/for-staff/StaffPage.jsx
+++ src/components/pages/for-staff/StaffPage.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 
 function StaffPage() {
 
-	const [auth, setAuth] = useState(false);
+	const [auth, setAuth] = useState('');
 
 	useEffect(() => {
 		let token = window.localStorage.getItem('token')
@@ -26,21 +26,27 @@ function StaffPage() {
 			  .then(function (response) {
 				if (response.data.status === "OK") 
 					{
-						setAuth(true)
+						setAuth('+')
 					}
 		  		})
 		  	.catch(function (error) {
 			  console.log(error);
+			  setAuth('-')
 		  	});
+		} else {
+			setAuth('-')
 		}
 	}, [setAuth])
 
-
-	return (
-		<>
-			{
-				auth ? 
-				(<div className={css.container}>
+	if (auth === '-') {
+		return (
+			<div className={css.container}>
+				<p className={css.forStaffText}>Эта страница доступна только авторизованным сотрудникам сервиса Bikee.<br />Пожалуйста, <Link to="/authorization">войдите</Link> или <Link to="/registration">зарегистрируйтесь</Link>.</p>
+			</div>
+		)
+	} else if (auth === '+') {
+		return (
+			<div className={css.container}>
 					<div className={css.information}>
 						<img src={priority_high} alt='Exclamation point' />
 						<h2 className={css.titleInformation}>Важная информация</h2>
@@ -49,14 +55,31 @@ function StaffPage() {
 						<p className={css.textInformation}>В связи с участившимися случаями кражи имущества (велосипедов) компании, было принято решение вести учет этих случаев, а так же назначить для каждого случая ответственного сотрудника, который будет уточнять у арендатора детали происшествия, консультировать по дальнейшим действиям и отслеживать прогресс.</p>
 						<p className={css.textInformation}>Чтобы увидеть полный список всех случаев краж, нажмите <Link to="/cases">сюда</Link> <br /> Список ответственных сотрудников находится <Link to="/employees">тут</Link></p>
 					</div>
-				</div>) 
-				: 
-				(<div className={css.container}>
-					<p className={css.forStaffText}>Эта страница доступна только авторизованным сотрудникам сервиса Bikee.<br />Пожалуйста, <Link to="/authorization">войдите</Link> или <Link to="/registration">зарегистрируйтесь</Link>.</p>
-				</div>)
-			}
-		</>
-	)
+				</div>
+		)
+	} else return (<div></div>)
+
+	// return (
+	// 	<>
+	// 		{
+	// 			auth === '+' ? 
+	// 			(<div className={css.container}>
+	// 				<div className={css.information}>
+	// 					<img src={priority_high} alt='Exclamation point' />
+	// 					<h2 className={css.titleInformation}>Важная информация</h2>
+	// 				</div>
+	// 				<div className={css.text}>
+	// 					<p className={css.textInformation}>В связи с участившимися случаями кражи имущества (велосипедов) компании, было принято решение вести учет этих случаев, а так же назначить для каждого случая ответственного сотрудника, который будет уточнять у арендатора детали происшествия, консультировать по дальнейшим действиям и отслеживать прогресс.</p>
+	// 					<p className={css.textInformation}>Чтобы увидеть полный список всех случаев краж, нажмите <Link to="/cases">сюда</Link> <br /> Список ответственных сотрудников находится <Link to="/employees">тут</Link></p>
+	// 				</div>
+	// 			</div>) 
+	// 			: 
+	// 			(<div className={css.container}>
+	// 				<p className={css.forStaffText}>Эта страница доступна только авторизованным сотрудникам сервиса Bikee.<br />Пожалуйста, <Link to="/authorization">войдите</Link> или <Link to="/registration">зарегистрируйтесь</Link>.</p>
+	// 			</div>)
+	// 		}
+	// 	</>
+	// )
 }
 
 export default StaffPage
