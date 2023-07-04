1278931498e70609649f61a994ffa574ea9b0c79
Polina <polina5523312@gmail.com>
2023-07-05 02:32:38 +0500

    exit button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -42,90 +42,12 @@ const Header = () => {
                 <div className={css.items} onClick={() => setIsMenuShown(!isMenuShown)}><Link to="/">Главная</Link></div>
                 <div className={css.items} onClick={() => setIsMenuShown(!isMenuShown)}><Link to="/help">Помощь</Link></div>
 				<div className={css.items} onClick={() => setIsMenuShown(!isMenuShown)}><Link to="/staff">Сотрудникам</Link></div>
-				{/* <p className={css.sprint}>Выйти</p> */}
+				<div className={css.items}>{status === 'resolved' && <Link to='/logout'>Выйти</Link>}</div>
             	</div>
             	}
 			</header>
 		</>
 	)
 }
-// const Header = () => {
-// 	const [auth, setAuth] = useState('');
-
-// 	useEffect(() => {
-// 		let token = window.localStorage.getItem('token')
-		
-// 		if (token !== 'undefined') {
-
-// 			let config = {
-// 				method: 'get',
-// 			  	maxBodyLength: Infinity,
-// 				url: 'https://sf-final-project-be.herokuapp.com/api/auth/',
-// 				headers: { 
-// 					"authorization": `Bearer ${token}`
-// 				},
-// 			}
-
-// 			axios(config)
-// 			  .then(function (response) {
-// 				if (response.data.status === "OK") 
-// 					{
-// 						setAuth('+')
-// 					}
-// 		  		})
-// 		  	.catch(function (error) {
-// 			  console.log(error);
-// 			  setAuth('-')
-// 		  	});
-// 		} else {
-// 			setAuth('-')
-// 		}
-// 	}, [setAuth])
-
-// 	if (auth === '-') {
-// 		return (
-// 			<header className={css.header}>
-// 				<div className={css.bikee}>
-// 				<img className={css.logo} src={Logo} alt='' />
-// 					<h1 className={css.title}>Bikee</h1>
-// 				</div>
-// 				<nav className={css.navigation}>
-// 					<Link to="/">Главная</Link>
-// 					<Link to="/help">Помощь</Link>
-// 					<Link to="/staff">Сотрудникам</Link>
-// 				</nav>
-// 				<span></span>
-// 			</header>
-// 		)
-// 	} else if (auth === '+') {
-// 		return (
-// 			<header className={css.header}>
-// 				<div className={css.bikee}>
-// 				<img className={css.logo} src={Logo} alt='' />
-// 					<h1 className={css.title}>Bikee</h1>
-// 				</div>
-// 				<nav className={css.navigation}>
-// 					<Link to="/">Главная</Link>
-// 					<Link to="/help">Помощь</Link>
-// 					<Link to="/staff">Сотрудникам</Link>
-// 				</nav>
-// 				<span>Выйти</span>
-// 			</header>
-// 		)
-// 	} else if (auth === '') {
-// 		<header className={css.header}>
-// 			<div className={css.bikee}>
-// 			<img className={css.logo} src={Logo} alt='' />
-// 				<h1 className={css.title}>Bikee</h1>
-// 			</div>
-// 				<nav className={css.navigation}>
-// 					<Link to="/">Главная</Link>
-// 					<Link to="/help">Помощь</Link>
-// 					<Link to="/staff">Сотрудникам</Link>
-// 				</nav>
-// 			<span></span>
-// 		</header>
-// 	}
-// }
 
 export default Header
